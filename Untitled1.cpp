74d90a15a82423cc39617952c0e95987513432db
uzairalee100 <Uzairalee100@gmail.com>
2024-09-23 12:28:07 +0500

    Adding files and assignment
    For Workshop

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	cout<<"Subject\t\tMarks\n" ;
+	cout<<"Maths\t\t95\n" ;
+	cout<<"English\t\t87\n" ;
+	cout<<"Programming\t90\n" ;
+	cout<<"Chemistry\t85" ;	
+	
+	
+	
+	return 0;
+	
+} 
